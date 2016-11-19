aa345febee24737ce498d82e7930265f33dc93aa
Neveralso <neveralso@gmail.com>
2016-11-19 17:40:07 +0000

    a

--- init.sql
+++ init.sql
@@ -3,11 +3,11 @@ CREATE DATABASE tencent_articles DEFAULT CHARSET utf8;
 
 DROP TABLE IF EXISTS tencent_articles.articles;
 CREATE TABLE tencent_articles.articles (
-  id           VARCHAR(20) NOT NULL PRIMARY KEY ,
-  category     VARCHAR(20),
-  sub_category VARCHAR(20),
-  date         VARCHAR(20),
-  href         VARCHAR(40),
+  id           VARCHAR(40) NOT NULL PRIMARY KEY ,
+  category     VARCHAR(40),
+  sub_category VARCHAR(40),
+  date         VARCHAR(40),
+  href         VARCHAR(100),
   title        TEXT,
   summary      TEXT,
   content      TEXT
